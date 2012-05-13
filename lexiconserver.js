dc5f696a2bdbf2a4d2ac52f22deddcf538e94b2f
cesine <cesine@yahoo.com>
2012-05-12 23:53:08 -0400

    reverting to even simpler node js

--- lexiconserver.js
+++ lexiconserver.js
@@ -1,18 +1,7 @@
-var express = require('express');
-var fs = require('fs');
-var _ = require('underscore');
-var app = express.createServer(express.logger());
-
-app.get('/',function(req,res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
- 	res.end('This is where the lexicon server is growing. The lexicon server has no public user interface. \n');
-});
-
-app.use(express.static(__dirname + '/public'));
-
-var port = process.env.PORT || 3001;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('This is where the lexicon server is growing. The lexicon server has no public user interface.\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
 
